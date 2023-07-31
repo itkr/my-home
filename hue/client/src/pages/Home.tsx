44cfa9deb07a648c3e8bc7960b42124d8dfd56c7
Shota Itakura <1531860+itkr@users.noreply.github.com>
2023-07-31 16:06:58 +0900

    色相を更新する

--- hue/client/src/pages/Home.tsx
+++ hue/client/src/pages/Home.tsx
@@ -1,4 +1,18 @@
-import { Box, Container, Heading, Switch } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Container,
+  Stack,
+  Heading,
+  HStack,
+  Flex,
+  Switch,
+} from "@chakra-ui/react";
+import { HuePicker, AlphaPicker } from "react-color";
 // import { api, v3 } from "node-hue-api";
 import { FC, useEffect, useState } from "react";
 import axios from "axios";
@@ -66,50 +80,135 @@ const useLights = () => {
     });
   };
 
+  const getLight = (deviceId: string) => {
+    const url = `http://${ip}/api/${username}/lights/${deviceId}`;
+    axios.get(url).then((res) => {
+      setLights((prev) => ({ ...prev, [deviceId]: res.data }));
+    });
+  };
+
   const toggleLight = (deviceId: string) => {
     const url = `http://${ip}/api/${username}/lights/${deviceId}/state`;
-    axios
-      .put(url, {
-        on: !lights[deviceId].state.on,
-        // bri: 254,
-        // hue: 10000,
-        // sat: 254,
-      })
-      .then((res) => {
-        getLights();
-      });
+    putLight(deviceId, { on: !lights[deviceId].state.on });
+  };
+
+  const putLight = (deviceId: string, state: Partial<Light["state"]>) => {
+    const url = `http://${ip}/api/${username}/lights/${deviceId}/state`;
+    axios.put(url, state).then((res) => {
+      getLights();
+    });
   };
 
   return {
     lights,
+    getLight,
     getLights,
+    putLight,
     toggleLight,
   };
 };
 
+function hslToHex(h: number, s: number, l: number): string {
+  console.log(h, s, l);
+  const hDecimal = l / 100;
+  const a = (s * Math.min(hDecimal, 1 - hDecimal)) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = hDecimal - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    // Convert to Hex and prefix with "0" if required
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0");
+  };
+  console.log(`#${f(0)}${f(8)}${f(4)}`);
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+const LightCard: FC<{
+  deviceId: string;
+  light: Light;
+  onToggle: () => void;
+}> = ({ deviceId, light, onToggle }) => {
+  const { toggleLight, putLight } = useLights();
+  const [color, setColor] = useState<string>("#fff");
+  return (
+    <Card>
+      <CardHeader>
+        <HStack spacing={3}>
+          <Avatar size="sm" name={light.name} bg="gray.500" />
+          <Heading as="h3" size="md">
+            {light.name}
+          </Heading>
+        </HStack>
+      </CardHeader>
+      <CardBody
+        bg={hslToHex(
+          Math.round((light.state.hue / 65535) * 360),
+          (light.state.sat / 254) * 100,
+          (light.state.bri / 254) * 100
+        )}
+      >
+        <Flex justifyContent="space-between" alignItems="center">
+          <Stack>
+            <HuePicker
+              color={{
+                h: (light.state.hue / 65535) * 360,
+                s: 254,
+                l: 254,
+                // s: light.state.sat,
+                // l: light.state.bri,
+              }}
+              onChangeComplete={(color) => {
+                console.log(color);
+                putLight(deviceId, {
+                  // hue: 32000,
+                  hue: Math.round((color.hsl.h / 360) * 65535),
+                  // sat: 254,
+                  // bri: 254,
+                });
+                // setColor(color.hex);
+              }}
+            />
+          </Stack>
+          <Switch
+            isChecked={light.state.on}
+            onChange={() => {
+              onToggle();
+            }}
+          />
+        </Flex>
+      </CardBody>
+      <CardFooter>Footer</CardFooter>
+    </Card>
+  );
+};
+
 const Home: FC = () => {
-  const { lights, getLights, toggleLight } = useLights();
+  const { lights, getLights, toggleLight, putLight } = useLights();
 
   useEffect(() => {
     getLights();
   }, []);
 
   return (
-    <Container>
-      <Heading as="h2"> Hello </Heading>
-      {Object.entries(lights).map(([key, value]) => {
-        return (
-          <Box key={key}>
-            <Heading as="h3"> {value.name} </Heading>
-            <Switch
-              isChecked={value.state.on}
-              onChange={() => {
-                toggleLight(key);
-              }}
-            />
-          </Box>
-        );
-      })}
+    <Container paddingY={3}>
+      <Stack spacing={3}>
+        <Heading as="h2"> Hello </Heading>
+        <Stack spacing={3}>
+          {Object.entries(lights).map(([key, value]) => {
+            return (
+              <LightCard
+                deviceId={key}
+                light={value}
+                key={key}
+                onToggle={() => {
+                  toggleLight(key);
+                }}
+              />
+            );
+          })}
+        </Stack>
+      </Stack>
     </Container>
   );
 };
