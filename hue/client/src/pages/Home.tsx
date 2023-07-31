5833e4ab9d2778d5aae94b918ac762082c61f281
Shota Itakura <1531860+itkr@users.noreply.github.com>
2023-07-31 12:16:50 +0900

    トグルボタン作成

--- hue/client/src/pages/Home.tsx
+++ hue/client/src/pages/Home.tsx
@@ -1,19 +1,115 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading, Switch } from "@chakra-ui/react";
 // import { api, v3 } from "node-hue-api";
 import { FC, useEffect, useState } from "react";
+import axios from "axios";
+
+type Light = {
+  name: string;
+  state: {
+    on: boolean;
+    bri: number;
+    hue: number;
+    sat: number;
+    effect: string;
+    xy: number[];
+    ct: number;
+    alert: string;
+    colormode: string;
+    mode: string;
+    reachable: boolean;
+  };
+  type: string;
+  modelid: string;
+  manufacturername: string;
+  productname: string;
+  capabilities: {
+    certified: boolean;
+    control: {
+      mindimlevel: number;
+      maxlumen: number;
+      colorgamuttype: string;
+      colorgamut: number[][];
+      ct: {
+        min: number;
+        max: number;
+      };
+    };
+    streaming: {
+      renderer: boolean;
+      proxy: boolean;
+    };
+  };
+  config: {
+    archetype: string;
+    function: string;
+    direction: string;
+    startup: {
+      mode: string;
+      configured: boolean;
+    };
+  };
+  uniqueid: string;
+  swversion: string;
+  swconfigid: string;
+  productid: string;
+};
+
+const useLights = () => {
+  const ip = "192.168.1.2";
+  const username = "9cjWI194Z58UwXDBKww9SMlZLrLH-0k01Gdjr1hv";
+  const [lights, setLights] = useState<Record<string, Light>>({});
+
+  const getLights = () => {
+    const url = `http://${ip}/api/${username}/lights`;
+    axios.get(url).then((res) => {
+      setLights(res.data);
+    });
+  };
+
+  const toggleLight = (deviceId: string) => {
+    const url = `http://${ip}/api/${username}/lights/${deviceId}/state`;
+    axios
+      .put(url, {
+        on: !lights[deviceId].state.on,
+        // bri: 254,
+        // hue: 10000,
+        // sat: 254,
+      })
+      .then((res) => {
+        getLights();
+      });
+  };
+
+  return {
+    lights,
+    getLights,
+    toggleLight,
+  };
+};
 
 const Home: FC = () => {
-  // const discovery = v3.discovery;
-  // const upnpSearch = discovery.upnpSearch;
-  // const discoveryResults = discovery.nupnpSearch().then((res) => {
-  //   console.log(res);
-  // });
+  const { lights, getLights, toggleLight } = useLights();
 
-  // const lights = api.lights.getAll();
+  useEffect(() => {
+    getLights();
+  }, []);
 
   return (
     <Container>
       <Heading as="h2"> Hello </Heading>
+      {Object.entries(lights).map(([key, value]) => {
+        return (
+          <Box key={key}>
+            <Heading as="h3"> {value.name} </Heading>
+            <Switch
+              isChecked={value.state.on}
+              onChange={() => {
+                toggleLight(key);
+              }}
+            />
+          </Box>
+        );
+      })}
     </Container>
   );
 };
