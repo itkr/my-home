13c7a1c92f72ac86bef741aea35beefc733f7345
Shota Itakura <1531860+itkr@users.noreply.github.com>
2024-02-25 00:10:08 +0900

    Update sidemenu

--- hue/client/src/components/SideMenu.tsx
+++ hue/client/src/components/SideMenu.tsx
@@ -6,24 +6,38 @@ import {
   Heading,
   Spacer,
   Stack,
+  Divider,
   Text,
   Link,
 } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/react";
+import {
+  FaCalendarAlt,
+  FaHome,
+  FaLightbulb,
+  FaObjectGroup,
+  FaSnowflake,
+  FaWrench,
+} from "react-icons/fa";
 
 const menuItems = [
-  { label: "Home", value: "/" },
-  { label: "Lights", value: "/lights" },
-  { label: "Groups", value: "/groups" },
-  { label: "Schedules", value: "/schedules" },
-  { label: "Scenes", value: "/scenes" },
-  { label: "Settings", value: "/settings" },
+  { label: "Home", value: "/", icon: <Icon as={FaHome} /> },
+  { label: "Lights", value: "/lights", icon: <Icon as={FaLightbulb} /> },
+  { label: "Groups", value: "/groups", icon: <Icon as={FaObjectGroup} /> },
+  {
+    label: "Schedules",
+    value: "/schedules",
+    icon: <Icon as={FaCalendarAlt} />,
+  },
+  { label: "Scenes", value: "/scenes", icon: <Icon as={FaSnowflake} /> },
+  { label: "Settings", value: "/settings", icon: <Icon as={FaWrench} /> },
 ];
 
 const SideMenu: FC = () => {
   return (
     <Flex backgroundColor="gray.100" w="250px" h="100vh" direction="column">
-      <Box p={4}>
-        <Flex direction="column" align="center">
+      <Box p={4} pt={8}>
+        <Flex direction="column" align="center" mb={8}>
           <Heading size="md" mb={2}>
             Hue Dashboard
           </Heading>
@@ -32,15 +46,20 @@ const SideMenu: FC = () => {
           </Text>
         </Flex>
         <Spacer />
+        <Divider borderColor="gray.300" />
         <Stack spacing={4} mt={8}>
           {menuItems.map((item) => (
-            <>
-              <Link key={item.value} href={item.value}>
-                {item.label}
-              </Link>
-            </>
+            <Link key={item.value} href={item.value}>
+              {item.icon} {item.label}
+            </Link>
           ))}
         </Stack>
+      </Box>
+      <Box p={4} mt="auto">
+        <Divider borderColor="gray.300" />
+        <Text fontSize="sm" color="gray.500" textAlign="center" pt={4}>
+          &copy; 2023 Hue Dashboard
+        </Text>
       </Box>
     </Flex>
   );
