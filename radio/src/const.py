354eda19477dda6637ebfda1461ce7a4dbf6e608
Shota Itakura <1531860+itkr@users.noreply.github.com>
2024-03-16 11:30:28 +0900

    Update radio

--- radio/src/const.py
+++ radio/src/const.py
@@ -12,27 +12,27 @@ REC_LIST = [
         'weekday': 6,  # Sunday
     },
     {
-        'name': '日向坂46松田好花の日向坂高校放送部',
+        'name': '僕が見たかった青空 君と奏でるラジオ',
         'broadcaster_id': 'LFR',  # ニッポン放送
         'start_time': '22:00:00',
-        'weekday': 5  # Saturday
+        'weekday': 5,  # Saturday
     },
     {
         'name': 'ローソン presents 日向坂46のほっとひといき！',
         'broadcaster_id': 'FMT',  # Tokyo FM
         'start_time': '11:30:00',
-        'weekday': 4  # Friday
+        'weekday': 4,  # Friday
     },
     {
         'name': 'ベルク presents 日向坂46の余計な事までやりましょう',
         'broadcaster_id': 'FMT',  # Tokyo FM
         'start_time': '20:00:00',
-        'weekday': 4  # Friday
+        'weekday': 4,  # Friday
     },
     {
         'name': '日向坂46潮紗理菜のサリマカシーラジオ',
         'broadcaster_id': 'INT',  # Tokyo FM
         'start_time': '23:30:00',
-        'weekday': 1  # Tuesday
+        'weekday': 1,  # Tuesday
     },
 ]
